10e3f61b2b7c9c4a55f0576e90fa9eec7288d060
Sergey <sergey.rymar.ne@yandex.ru>
2020-02-04 20:33:15 +0200

    Initial commit,homework 1

---
+++ homeWork_1.go
@@ -0,0 +1,44 @@
+package main
+
+type human struct {
+	firstName  string
+	secondName string
+	age        uint8
+}
+
+type worker struct {
+	human
+	salary int64
+}
+type doctor struct {
+	worker
+	Specialty      string   `json:"specialty" xml:"specialty"`
+	ListOfPatients []string `json:"list_of_patients,omitempty" xml:"list_of_patients"`
+}
+type manager struct {
+	worker
+	Department  string `json:"department" xml:"department"`
+	CompanyName string `json:"company_name" xml:"company_name"`
+}
+
+type sportsmen struct {
+	worker
+	Sport string `json:"sport" xml:"sport"`
+	Score string `json:"score" xml:"score"`
+}
+type programmer struct {
+	worker
+	Languages []string `json:"languages" xml:"languages"`
+}
+type teacher struct {
+	worker
+	Field            string `json:"field" xml:"field"`
+	NumberOfStudents int    `json:"number_of_students" xml:"number_of_students"`
+}
+
+
+func main() {
+
+
+
+}
