10eaa8a9651f2a02cdeacca602b5c2fa563e5614
Sergey <sergey.rymar.ne@yandex.ru>
2020-02-04 22:07:08 +0200

    second task,pointers and call happyBirthday()

--- homeWork_1.go
+++ homeWork_1.go
@@ -1,56 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type human struct {
 	firstName  string
 	secondName string
-	age        uint8
+	age        int
+}
+
+func (receiver *human) happyBirthday() {
+	fmt.Println("happyBirthday() from human ", time.Now().Year()-receiver.age)
+}
+func (receiver *human) sayName() {
+	fmt.Printf("My name is %s %s", receiver.firstName, receiver.secondName)
 }
 
 type worker struct {
-	human
+	*human
 	salary int64
 }
+
+/*func (receiver *worker) happyBirthday() {
+	fmt.Println("happyBirthday() from  worker ",time.Now().Year() - receiver.age)
+}*/
+
 type doctor struct {
-	worker
+	*worker
 	Specialty      string   `json:"specialty" xml:"specialty"`
 	ListOfPatients []string `json:"list_of_patients,omitempty" xml:"list_of_patients"`
 }
+
 type manager struct {
-	worker
+	*worker
 	Department  string `json:"department" xml:"department"`
 	CompanyName string `json:"company_name" xml:"company_name"`
 }
 
 type sportsmen struct {
-	worker
+	*worker
 	Sport string `json:"sport" xml:"sport"`
 	Score string `json:"score" xml:"score"`
 }
 type programmer struct {
-	worker
+	*worker
 	Languages []string `json:"languages" xml:"languages"`
 }
 type teacher struct {
-	worker
+	*worker
 	Field            string `json:"field" xml:"field"`
 	NumberOfStudents int    `json:"number_of_students" xml:"number_of_students"`
 }
 
 func main() {
-	callPanic()
-	callPanic1()
+	mrBeen := doctor{
+		worker: &worker{&human{
+			firstName:  "Jac",
+			secondName: "Been",
+			age:        45,
+		}, 4500},
+		Specialty:      "surgeon",
+		ListOfPatients: []string{"mrs.Tool"},
+	}
+	fmt.Printf("%+v\n", mrBeen.human.secondName)
 
-}
-
-func callPanic1() {
-	var panicProgramer *programmer
-	panicProgramer.Languages = []string{"Go", "Java", "C++"}
-	fmt.Printf("I know the following languages %v", panicProgramer.Languages)
-}
-
-func callPanic() {
-	var panicTeacher *teacher
-	panicTeacher.NumberOfStudents = 2
+	mrBeen.worker.happyBirthday()
+	mrBeen.worker.human.happyBirthday()
+	mrBeen.worker.sayName()
+	mrBeen.worker.human.sayName()
 }
