05982cad47fffbd2433e22a2036e8d31124a64d5
Sergey <sergey.rymar.ne@yandex.ru>
2020-02-11 21:43:13 +0200

    implemented interface worker

--- homeWork_1.go
+++ homeWork_1.go
@@ -6,39 +6,96 @@ type human struct {
 	age        uint8
 }
 
-type worker struct {
-	human
-	salary int64
+type worker interface {
+	getProfession() string
+	getPosition() string
+	getName() string
 }
 type doctor struct {
-	worker
-	Specialty      string   `json:"specialty" xml:"specialty"`
-	ListOfPatients []string `json:"list_of_patients,omitempty" xml:"list_of_patients"`
+	human
+	Profession string `json:"specialty" xml:"specialty"`
+	Position   string `json:"list_of_patients,omitempty" xml:"list_of_patients"`
 }
+
+func (resiver doctor) getProfession() string {
+	return resiver.Profession
+}
+func (resiver doctor) getPosition() string {
+	return resiver.Position
+}
+func (resiver doctor) getName() string {
+	return resiver.firstName
+}
+
 type manager struct {
-	worker
-	Department  string `json:"department" xml:"department"`
-	CompanyName string `json:"company_name" xml:"company_name"`
+	human
+	Profession string `json:"specialty" xml:"specialty"`
+	Position   string `json:"list_of_patients,omitempty" xml:"list_of_patients"`
+}
+
+func (resiver manager) getProfession() string {
+	return resiver.Profession
+}
+func (resiver manager) getPosition() string {
+	return resiver.Position
+}
+func (resiver manager) getName() string {
+	return resiver.firstName
 }
 
 type sportsmen struct {
-	worker
-	Sport string `json:"sport" xml:"sport"`
-	Score string `json:"score" xml:"score"`
+	human
+	Sport      string `json:"sport" xml:"sport"`
+	Score      string `json:"score" xml:"score"`
+	Profession string `json:"specialty" xml:"specialty"`
+	Position   string `json:"list_of_patients,omitempty" xml:"list_of_patients"`
 }
+
+func (resiver sportsmen) getProfession() string {
+	return resiver.Profession
+}
+func (resiver sportsmen) getPosition() string {
+	return resiver.Position
+}
+func (resiver sportsmen) getName() string {
+	return resiver.firstName
+}
+
 type programmer struct {
-	worker
-	Languages []string `json:"languages" xml:"languages"`
+	human
+	Languages  []string `json:"languages" xml:"languages"`
+	Profession string   `json:"specialty" xml:"specialty"`
+	Position   string   `json:"list_of_patients,omitempty" xml:"list_of_patients"`
 }
+
+func (resiver programmer) getProfession() string {
+	return resiver.Profession
+}
+func (resiver programmer) getPosition() string {
+	return resiver.Position
+}
+func (resiver programmer) getName() string {
+	return resiver.firstName
+}
+
 type teacher struct {
-	worker
+	human
 	Field            string `json:"field" xml:"field"`
 	NumberOfStudents int    `json:"number_of_students" xml:"number_of_students"`
+	Profession       string `json:"specialty" xml:"specialty"`
+	Position         string `json:"list_of_patients,omitempty" xml:"list_of_patients"`
 }
 
+func (resiver teacher) getProfession() string {
+	return resiver.Profession
+}
+func (resiver teacher) getPosition() string {
+	return resiver.Position
+}
+func (resiver teacher) getName() string {
+	return resiver.firstName
+}
 
 func main() {
-
-
 
 }
