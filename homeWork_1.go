c84ff90a932da2cad6befaf8073f1387e33e84d7
Sergey <sergey.rymar.ne@yandex.ru>
2020-02-11 22:38:52 +0200

    add func returnType()

--- homeWork_1.go
+++ homeWork_1.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type human struct {
 	firstName  string
 	secondName string
@@ -63,9 +68,8 @@ func (resiver sportsmen) getName() string {
 
 type programmer struct {
 	human
-	Languages  []string `json:"languages" xml:"languages"`
-	Profession string   `json:"specialty" xml:"specialty"`
-	Position   string   `json:"list_of_patients,omitempty" xml:"list_of_patients"`
+	Profession string `json:"specialty" xml:"specialty"`
+	Position   string `json:"list_of_patients,omitempty" xml:"list_of_patients"`
 }
 
 func (resiver programmer) getProfession() string {
@@ -97,5 +101,75 @@ func (resiver teacher) getName() string {
 }
 
 func main() {
+	workerBoolMap := make(map[worker]bool, 5)
+	doctor1 := doctor{
+		human: human{
+			firstName:  "Sergey",
+			secondName: "Rymar",
+			age:        34,
+		},
+		Profession: "Doctor",
+		Position:   "1",
+	}
+	workerBoolMap[doctor1] = true
+	manager1 := manager{
+		human: human{
+			firstName:  "Denis",
+			secondName: "Denisich",
+			age:        18,
+		},
+		Profession: "manager",
+		Position:   "2",
+	}
+	workerBoolMap[manager1] = true
+	sportsmen1 := sportsmen{
+		human: human{
+			firstName:  "Pavel",
+			secondName: "Pavelovich",
+			age:        25,
+		},
+		Sport:      "wrestling",
+		Score:      "4",
+		Profession: "sportsmen",
+		Position:   "3",
+	}
+	workerBoolMap[sportsmen1] = true
+	programmer1 := programmer{
+		human: human{
+			firstName:  "Sergey",
+			secondName: "Sergeevich",
+			age:        50,
+		},
+		Profession: "programmer",
+		Position:   "4",
+	}
+	workerBoolMap[programmer1] = true
+	teacher1 := teacher{
+		human: human{
+			firstName:  "Stas",
+			secondName: "Stasovich",
+			age:        23,
+		},
+		Field:            "Go",
+		NumberOfStudents: 4,
+		Profession:       "teacher",
+		Position:         "5",
+	}
+	workerBoolMap[teacher1] = true
 
+	for key, _ := range workerBoolMap {
+		fmt.Println("Name=", key.getName(), " Profession=", key.getProfession(), " Position=", key.getPosition())
+	}
+
+	fmt.Println(" ===================================================")
+	for key, value := range returnType(workerBoolMap) {
+		fmt.Println(" Profession=", key, " Type=", value)
+	}
+}
+func returnType(cash map[worker]bool) map[string]reflect.Type {
+	result := make(map[string]reflect.Type)
+	for key, _ := range cash {
+		result[key.getProfession()] = reflect.TypeOf(key)
+	}
+	return result
 }
